8fe61f2b194a12e2e4a18c8359844a60eebc9c4a
Kyle Johnsen <kjjohnsen@gmail.com>
2022-05-21 14:33:21 -0400

    added some infrastructure for leap, not done yet

---
+++ Assets/LeapShader.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+public class LeapShader : MonoBehaviour
+{
+    public Shader awesomeShader = null;
+    private Material m_renderMaterial;
+
+    void Start()
+    {
+        if (awesomeShader == null)
+        {
+            Debug.LogError("no awesome shader.");
+            m_renderMaterial = null;
+            return;
+        }
+        m_renderMaterial = new Material(awesomeShader);
+    }
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, m_renderMaterial);
+    }
+}
